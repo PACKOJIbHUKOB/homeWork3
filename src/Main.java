0aa1e90364bec95cb295802f8833916b20da22b6
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-06-01 08:09:35 +0300

    Мастер ветка домашней работы 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {   task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();}
+    private static void task1() {
+        System.out.println("задача 1");
+        byte one = 127;
+        short two = 32767;
+        int three = 2147483647;
+        long four = 9223372036854775807L;
+        float five = 4.21564651F;
+        double six =351654.5654661588;
+        System.out.println("Значение переменной one с типом byte равно " + one);
+        System.out.println("Значение переменной two с типом short равно " + two);
+        System.out.println("Значение переменной three с типом int равно " + three);
+        System.out.println("Значение переменной four с типом long равно " + four);
+        System.out.println("Значение переменной five с типом float равно " + five);
+        System.out.println("Значение переменной six с типом double равно " + six);}
+    private static void task2() {
+        System.out.println("задача 2");
     }
+    private static void task3() {
+        System.out.println("задача 3");}
+    private static void task4() {
+        System.out.println("задача 4");}
+
+    private static void task5() {
+        System.out.println("задача 5");}
+
+    private static void task6() {
+        System.out.println("задача 6");}
+
+    private static void task7() {
+        System.out.println("задача 7");}
+
+    private static void task8() {
+        System.out.println("задача 8");}
 }
